316952b35f76d2aece62b41be66a48497752f08a
yuhandbcontext <yuhandbcontext@noreply.gitcode.com>
2026-01-11 09:17:25 +0800

    工作目录以及Converter

---
+++ ChuanLeMaClient/Converters/FolderOpenVisibilityConverter.cs
@@ -0,0 +1,32 @@
+﻿using Avalonia.Data.Converters;
+using ChuanLeMaClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChuanLeMaClient.Converters
+{
+    internal class FolderOpenVisibilityConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is List<TagInfo> infos)
+            {
+                if (infos.Any() && infos.Any(o => o.Name == "目录")) return true;
+                return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
