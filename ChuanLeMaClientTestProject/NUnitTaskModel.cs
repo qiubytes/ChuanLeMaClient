0af7c16b572b9c9077599c4595801ccf544c3796
qiubytes <yuhandev@163.com>
2026-01-18 18:55:25 +0800

    集成sqlite3 以及帮助类

---
+++ ChuanLeMaClientTestProject/NUnitTaskModel.cs
@@ -0,0 +1,21 @@
+﻿using ChuanLeMaClient.Repository;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace ChuanLeMaClientTestProject;
+
+public class NUnitTaskModel
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task Test1()
+    {
+        SQLiteHelper liteHelper = new SQLiteHelper();
+        DataTable dt = await liteHelper.QueryAsync("select * from TaskModel;", new Microsoft.Data.Sqlite.SqliteParameter[] { });
+        Assert.Pass();
+    }
+}
