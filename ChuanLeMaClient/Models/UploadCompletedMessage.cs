d680a26ef35e9fd1d4508519c911c3a24409fe4d
qiubytes <yuhandev@163.com>
2026-01-24 12:47:43 +0800

    上传完成消息

---
+++ ChuanLeMaClient/Models/UploadCompletedMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChuanLeMaClient.Models
+{
+    /// <summary>
+    /// 上传完成消息
+    /// </summary>
+    /// <param name="taskid"></param>
+    /// <param name="localfilepath"></param>
+    /// <param name="remotefilepath"></param>
+    /// <param name="progress"></param>
+    public record UploadCompletedMessage(string taskid, string localfilepath, string remotefilepath);
+
+
+}
