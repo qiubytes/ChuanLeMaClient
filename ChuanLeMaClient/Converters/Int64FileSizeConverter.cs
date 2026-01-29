ed510dabb0d11a690dda492c793f78d4961bdfad
qiubytes <yuhandev@163.com>
2026-01-29 21:34:32 +0800

    任务界面  文件大小转换器

---
+++ ChuanLeMaClient/Converters/Int64FileSizeConverter.cs
@@ -0,0 +1,46 @@
+﻿using Avalonia.Data.Converters;
+using ChuanLeMaClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ChuanLeMaClient.Converters
+{
+    public class Int64FileSizeConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is Int64 size)
+            {
+
+                //单位 B
+                if (size < 1024)
+                {
+                    return $"{size} B";
+                }
+                else if (size < 1024 * 1024)
+                {
+                    return $"{(size / 1024.0):F2} KB";
+                }
+                else if (size < 1024 * 1024 * 1024)
+                {
+                    return $"{(size / (1024.0 * 1024.0)):F2} MB";
+                }
+                else
+                {
+                    return $"{(size / (1024.0 * 1024.0 * 1024.0)):F2} GB";
+                } 
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return "";
+        }
+    }
+}
