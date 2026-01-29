f07bcd599d887606e5db9b9551901ed6034f19e2
qiubytes <yuhandev@163.com>
2026-01-29 22:02:28 +0800

    LocalFullPathToFileNameConverter

---
+++ ChuanLeMaClient/Converters/LocalFullPathToFileNameConverter.cs
@@ -0,0 +1,32 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ChuanLeMaClient.Converters
+{
+    /// <summary>
+    /// 文件全路径转换为文件名
+    /// </summary>
+    public class LocalFullPathToFileNameConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is string fullname)
+            {
+                string filename = System.IO.Path.GetFileName(fullname);
+                return filename;
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return "";
+        }
+    }
+}
